93e8ecc3c4b79d4e3fbb826657e6b5927b66313e
jediminer543 <jediminer543@users.noreply.github.com>
2022-06-30 01:09:28 +0100

    Fixed issues with build

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 set -e
-exec "php /bootstrap/bootstrap-db.php"
+php /bootstrap/bootstrap-db.php
 if [[ ! -z "${GOD_NAME}" ]] && [[ ! -z "${GOD_PASS}" ]]; then
-    exec "php /var/www/mediawiki/w/maintenance/createAndPromote.php ${GOD_NAME} ${GOD_PASS} --bureaucrat --sysop --interface-admin --force"
+    php /var/www/mediawiki/w/maintenance/createAndPromote.php "${GOD_NAME}" "${GOD_PASS}" --bureaucrat --sysop --interface-admin --force
 fi
 envsubst '$URL_PREFIX' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 exec "$@"
