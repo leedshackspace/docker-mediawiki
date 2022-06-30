cb5d43b731d5818ed39151bd527f7867f5cb6470
jediminer543 <jediminer543@users.noreply.github.com>
2022-06-30 01:18:21 +0100

    Forgot a semicolon;

--- config/build/LocalSettings.php
+++ config/build/LocalSettings.php
@@ -3,5 +3,5 @@
 if (file_exists("/config/LocalSettings.php")) {
     require_once "/config/LocalSettings.php";
 } else {
-    require_once "/config-bake/LocalSettings.php"
+    require_once "/config-bake/LocalSettings.php";
 }
