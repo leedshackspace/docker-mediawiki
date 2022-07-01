2b904230acf8d836dbaad4dbf9806a3b9d10c275
jediminer543 <jediminer543@users.noreply.github.com>
2022-07-01 17:52:04 +0100

    I FOUND IT!!!

--- bootstrap/bootstrap-db.php
+++ bootstrap/bootstrap-db.php
@@ -8,12 +8,13 @@ $wgDBport = getenv("DB_PORT") ?: "5432";
 $wgDBmwschema = getenv("DB_SCHEMA") ?: "mediawiki";
 
 $queryBaseFile = "/var/www/mediawiki/w/maintenance/postgres/tables.sql";
-# TASTY SQLi "SET search_path TO $wgDBmwschema;".
 $queryBase = file_get_contents($queryBaseFile);
 
 $queryGenFile = "/var/www/mediawiki/w/maintenance/postgres/tables-generated.sql";
-# TASTY SQLi "SET search_path TO $wgDBmwschema;".
 $queryGen = file_get_contents($queryGenFile);
+
+$queryUpdateFile = "/var/www/mediawiki/w/maintenance/postgres/update-keys.sql";
+$queryUpdate = file_get_contents($queryUpdateFile);
 
 $pgv_connection_string = "host=$wgDBserver dbname=$wgDBname port=$wgDBport user=$wgDBuser password=$wgDBpassword";
 $pgv_connection = pg_connect($pgv_connection_string);
@@ -32,14 +33,11 @@ if ($pgv_connection == false) {
 # TASTY SQLi
 $result = pg_query($pgv_connection, "CREATE SCHEMA IF NOT EXISTS $wgDBmwschema; SET search_path TO $wgDBmwschema;");
 
-#echo "Issuing \n $queryBase\n";
 echo "Issuing tables.sql\n";
 $result = pg_query($pgv_connection, $queryBase);
-#var_dump($result);
 if ($result == false) {
     echo "Base setup failed rolling back\n";
     $result = pg_query($pgv_connection, "ROLLBACK;");
-    #var_dump($result);
     if ($result == false) {
         echo "Rollback failed?!?\n";
     }
@@ -49,14 +47,11 @@ if ($result == false) {
     if ($result == false) {
         echo "Commit failed?!?\n";
     }
-    #echo "Issuing \n $queryGen\n";
     echo "Issuing tables-generated.sql\n";
     $result = pg_query($pgv_connection, $queryGen);
-    #var_dump($result);
     if ($result == false) {
         echo "Extras setup failed rolling back\n";
         $result = pg_query($pgv_connection, "ROLLBACK;");
-        #var_dump($result);
         if ($result == false) {
             echo "Rollback failed?!?\n";
         }
@@ -65,6 +60,21 @@ if ($result == false) {
         $result = pg_query($pgv_connection, "COMMIT;");
         if ($result == false) {
             echo "Commit failed?!?\n";
+        }
+        echo "Issuing update-keys.sql\n";
+        $result = pg_query($pgv_connection, $queryUpdate);
+        if ($result == false) {
+            echo "Update keys setup failed rolling back\n";
+            $result = pg_query($pgv_connection, "ROLLBACK;");
+            if ($result == false) {
+                echo "Rollback failed?!?\n";
+            }
+        } else {
+            echo "Committing\n";
+            $result = pg_query($pgv_connection, "COMMIT;");
+            if ($result == false) {
+                echo "Commit failed?!?\n";
+            }
         }
     }
 }
